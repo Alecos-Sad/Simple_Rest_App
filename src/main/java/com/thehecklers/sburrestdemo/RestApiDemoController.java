5bf3e9a5a5a6c0639749dee25434004902310f87
Sadonikov Alexander <alecos100@gmail.com>
2023-08-09 21:02:59 +0300

    3. Работа с бд

--- src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
+++ src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
@@ -4,62 +4,43 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/coffees")
 public class RestApiDemoController {
-    private final List<Coffee> coffees = new ArrayList<>();
+    private final CoffeeRepository coffeeRepository;
 
-    public RestApiDemoController(List<Coffee> coffees) {
-        this.coffees.addAll(List.of(new Coffee("Cafe Cereza"),
-                new Coffee("Cafe Ganador"),
-                new Coffee("Cafe Lareno"),
-                new Coffee("Cafe Tres Pontas")
-        ));
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
     }
 
     @GetMapping()
     public Iterable<Coffee> getCoffees() {
-        return coffees;
+        return coffeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id) {
-        for (Coffee coffee : coffees) {
-            if (coffee.getId().equals(id)) {
-                return Optional.of(coffee);
-            }
-        }
-        return Optional.empty();
+        return coffeeRepository.findById(id);
     }
 
     @PostMapping()
     Coffee postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-        return coffee;
+        return coffeeRepository.save(coffee);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        int coffeeIndex = -1;
-        for (Coffee c : coffees) {
-            if (c.getId().equals(id)) {
-                coffeeIndex = coffees.indexOf(c);
-                coffees.set(coffeeIndex, coffee);
-            }
-        }
-        return (coffeeIndex == -1) ?
-                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-                new ResponseEntity<>(coffee, HttpStatus.OK);
+        return (coffeeRepository.existsById(id))
+                ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED)
+                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     void deleteCoffee(@PathVariable String id) {
-        coffees.removeIf(coffee -> coffee.getId().equals(id));
+        coffeeRepository.deleteById(id);
     }
 }
 
--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -11,35 +11,5 @@ public class SburRestDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SburRestDemoApplication.class, args);
 	}
-
 }
 
-class Coffee{
-	private String id;
-	private String name;
-
-	public Coffee(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public Coffee(String name) {
-		this(UUID.randomUUID().toString(), name);
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-}
